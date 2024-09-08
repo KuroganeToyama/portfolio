faa49e1af767fbec4c2f3e02c80f5e7cfbbee264
KuroganeToyama <dingyen96@gmail.com>
2024-09-08 19:06:20 +0000

    Added social info

---
+++ src/data/socials.tsx
@@ -0,0 +1,21 @@
+import { FiGithub, FiLinkedin, FiGlobe } from "react-icons/fi";
+  
+export const socials = [
+    {
+        id: 1,
+        icon: <FiGlobe />,
+        url: "https://www.mrkhunt.me/",
+    },
+
+    {
+        id: 2,
+        icon: <FiGithub />,
+        url: "https://github.com/KuroganeToyama",
+    },
+    
+    {
+        id: 3,
+        icon: <FiLinkedin />,
+        url: "https://www.linkedin.com/in/di-nguyen-pf",
+    },
+];
