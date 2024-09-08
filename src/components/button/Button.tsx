7f9c8ea7898c4b02e6a459cca37b53afdc3cedaf
KuroganeToyama <dingyen96@gmail.com>
2024-09-08 19:58:05 +0000

    Added button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+interface ButtonProps {
+  label: string;
+  path: string;
+}
+
+const Button = ({ label, path }: ButtonProps) => {
+  return (
+    <a href={path}>
+      <button className="group relative flex w-fit items-center justify-center overflow-hidden rounded-md bg-blue-300 text-zinc-800  px-4 py-2 font-bold transition-transform ease-out  hover:scale-105">
+        <span className="absolute inset-0 z-0 h-full translate-y-9 bg-zinc-100 transition-transform duration-300 ease-in-out group-hover:translate-y-0" />
+        <span className="relative flex items-center justify-center gap-2">
+          {label} <FaExternalLinkAlt />{" "}
+        </span>
+      </button>
+    </a>
+  );
+};
+
+export default Button;
