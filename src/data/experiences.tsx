6bd3eedec76f687c5ebe1df4776f2b1d65f2a35c
KuroganeToyama <dingyen96@gmail.com>
2024-09-09 01:55:14 +0000

    Added past experiences

---
+++ src/data/experiences.tsx
@@ -0,0 +1,37 @@
+export const experiences = [
+    {
+        company: "School of Computer Science",
+        role: "Part Time Instructional Support Assistant",
+        date: "Sep 2024 - Present",
+        location: "Waterloo, CA",
+        desc: [
+            "Looking over new course website and automarking process",
+            "Leading the autotesting team to properly handle automarking",
+            "Assisting full-time ISAs with all technical work",
+        ],
+    },
+
+    {
+        company: "School of Computer Science",
+        role: "Part Time Instructional Support Assistant",
+        date: "Jan 2024 - Apr 2024",
+        location: "Waterloo, CA",
+        desc: [
+            "Rennovated course website to accomodate additional course materials",
+            "Assisted in streamlining automarking processes",
+            "Mentored full-time ISA and assisted them with their job",
+        ],
+    },
+
+    {
+        company: "School of Computer Science",
+        role: "Full Time Instructional Support Assistant",
+        date: "Sep 2023 - Dec 2023",
+        location: "Waterloo, CA",
+        desc: [
+            "Configured Linux server and marking engine to test student submissions",
+            "Leveraged Python and SQLite to manage student participation data",
+            "Provided academic support to students through office hours and tutorials",
+        ],
+    },
+];
