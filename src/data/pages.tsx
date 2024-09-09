e8652419280ca7ef0e78c2b6a597335fb152b8c4
KuroganeToyama <dingyen96@gmail.com>
2024-09-09 04:12:18 +0000

    Added Home tab

--- src/data/pages.tsx
+++ src/data/pages.tsx
@@ -1,4 +1,5 @@
 export const pages = [
+    { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Experience", path: "/experience" },
     { name: "Projects", path: "/projects" },
