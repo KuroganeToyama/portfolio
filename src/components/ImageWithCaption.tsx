8c146c1c9e6b69e13609c824db7a1a5a72d7228b
KuroganeToyama <dingyen96@gmail.com>
2024-09-10 04:57:05 +0000

    Ensured caption fits the image with

--- src/components/ImageWithCaption.tsx
+++ src/components/ImageWithCaption.tsx
@@ -9,7 +9,7 @@ interface ImgageWithCaptionProps {
 
 const ImgageWithCaption = ({ src, alt, styles, caption }: ImgageWithCaptionProps) => {
     return (
-      <figure>
+      <figure className="flex flex-col items-left w-full max-w-sm">
         <img src={src} alt={alt} className={styles} />
         <figcaption className="pr-8 text-base leading-8 font-normal text-gray-800">{caption}</figcaption>
       </figure>
