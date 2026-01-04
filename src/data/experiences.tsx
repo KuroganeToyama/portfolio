2d7aa809f79ad92cfaef8a58a60038424adcb7d4
Di Nguyen <dingyen96@gmail.com>
2026-01-04 16:00:31 -0500

    updated experiences

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -1,11 +1,34 @@
 export const experiences = [
     {
+        company: "Genesys Cloud",
+        role: "Full Stack Software Developer",
+        date: "Sep 2025 - Dec 2025",
+        location: "Toronto, CA",
+        desc: [
+            "Improved performance and reduced compute cost in AWS Lambda services",
+            "Redesgined service health reports in AWS Bedrock",
+            "Maintained crucial APIs for public and internal use",
+        ],
+    },
+    {
+        company: "WatSPEED",
+        role: "Technical Solutions Developer",
+        date: "Jan 2025 - Apr 2025",
+        location: "Waterloo, CA",
+        desc: [
+            "Developed automation tools and data processing tools for internal use",
+            "Investigated and resolved inconsistencies from data migration",
+            "Refactored codebase and optimized SQL queries",
+        ],
+    },
+    {
         company: "School of Computer Science",
         role: "Part Time Instructional Support Assistant",
-        date: "Sep 2024 - Present",
+        date: "Sep 2024 - Dec 2024",
         location: "Waterloo, CA",
         desc: [
             "Looking over new course website and automarking process",
+            "Developed tools to automate student participation data processing",
             "Leading the autotesting team to properly handle automarking",
             "Assisting full-time ISAs with all technical work",
         ],
